f03ba4e2c361577127442142313e5ee62fe53ed7
hamedabdy <freeze2ice@gmail.com>
2025-04-12 13:22:32 +0200

    clean up and moving to new home

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -1,23 +1,11 @@
 import React, { useEffect, Suspense, lazy } from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { Typography } from "@mui/material";
+
 import Home from "./components/Home";
-import DynamicList from "./components/DynamicList";
-
-import // Container,
-// Paper,
-// Typography,
-// AppBar,
-// Toolbar,
-// Grid,
-// Drawer,
-// List,
-// ListItem,
-// ListItemText,
-// Box,
-"@mui/material";
-
 const DynamicForm = lazy(() => import("./components/DynamicForm"));
-const TableList = lazy(() => import("./components/TableList"));
+const DynamicList = lazy(() => import("./components/DynamicList"));
+// const TableList = lazy(() => import("./components/TableList"));
 const PaperBase = lazy(() => import("./newHome/Paperbase.js"));
 
 function App() {
@@ -25,20 +13,19 @@ function App() {
 
   return (
     <Router>
-      <div>
+      <Suspense fallback={<Typography variant="h5">Loading...</Typography>}>
         <div id="container">
           {/* Content will be dynamically replaced based on the route */}
-          {/* <Suspense fallback={<Typography variant="h5">Loading...</Typography>}> */}
+
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/paperbase/*" element={<PaperBase />} />
-            <Route path="/table_list" element={<TableList />} />
+            {/* <Route path="/table_list" element={<TableList />} /> */}
             <Route path="/:tableName.list" element={<DynamicList />} />
             <Route path="/:tableName.form" element={<DynamicForm />} />
           </Routes>
-          {/* </Suspense> */}
         </div>
-      </div>
+      </Suspense>
     </Router>
   );
 }
