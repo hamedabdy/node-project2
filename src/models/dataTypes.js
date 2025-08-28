00703c277abf20adc28d04bc0fc9e3da72703cb8
Hamed <freeze2ice@gmail.com>
2025-08-28 13:01:03 +0200

    added data type mapping decimal

--- src/models/dataTypes.js
+++ src/models/dataTypes.js
@@ -3,6 +3,7 @@ const { DataTypes } = require("sequelize");
 const sequelizeDataTypes = {
   string: (max_length) => DataTypes.STRING(max_length),
   integer: (max_length) => DataTypes.INTEGER(max_length),
+  decimal: (max_length) => DataTypes.DECIMAL(max_length),
   boolean: DataTypes.BOOLEAN,
   date: (max_length) => DataTypes.DATEONLY(max_length),
   datetime: (max_length) => DataTypes.DATE(max_length),
